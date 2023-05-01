341192af878aa2a217c1c0d8f05edf78599c5a7c
buzap <hibuzap@gmail.com>
2023-05-01 23:59:22 +0800

    fix nextjs "no viewport meta tag in  _document" warning

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-                <meta name="description" content="See what's happening in a github repository at a glance" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+            <Head />
             <body>
                 <Main />
                 <NextScript />
