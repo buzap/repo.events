309aef6fa7678d9b0fbc1466bba5cb74b8390a83
buzap <hibuzap@gmail.com>
2023-05-04 02:25:25 +0800

    MVP of the home page

---
+++ packages/web/components/RepoInput/RepoInput.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { RepoInput } from './RepoInput'
+
+export default {
+    title: 'components/RepoInput',
+    component: RepoInput,
+} as Meta<typeof RepoInput>
+
+type Story = StoryObj<typeof RepoInput>
+
+export const Default: Story = {}
--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,3 +1,18 @@
+import { Box } from '@primer/react'
+import { Intro } from '@/components/Intro'
+import { RepoInput } from '@/components/RepoInput'
+
 export default function Home() {
-    return <h1>Hello World</h1>
+    const onChange = (e: any) => {
+        console.log(e)
+    }
+
+    return (
+        <Box sx={{ minHeight: '100%', backgroundColor: 'canvas.default' }}>
+            <Intro />
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <RepoInput onChange={onChange} sx={{ width: '750px', maxWidth: '90vw' }} />
+            </Box>
+        </Box>
+    )
 }
