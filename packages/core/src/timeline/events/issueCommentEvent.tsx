a2934b5a7a17d30f384b67221dcd5acff9d45373
buzap <hibuzap@gmail.com>
2023-05-05 01:50:01 +0800

    setup build tools for the core package

--- packages/core/src/timeline/events/issueCommentEvent.tsx
+++ packages/core/src/timeline/events/issueCommentEvent.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { SxProp, StyledOcticon, Link } from '@primer/react'
 import { CommentIcon } from '@primer/octicons-react'
 import { GithubEvent } from '../../types/github'
