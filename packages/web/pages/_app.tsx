15b316f6e287755d3210c757ec93c13ae464c9f8
Shawn Chen <shawn800thz@gmail.com>
2023-05-18 15:11:44 +0800

    fix nextjs "no viewport meta tag in  _document" warning

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,14 +1,22 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ThemeProvider } from '@primer/react'
 import { Layout } from '@/components/layout'
 import './_app.css'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </ThemeProvider>
+        <>
+            <Head>
+                <meta name="viewport" content="viewport-fit=cover" />
+                <meta name="description" content="See what's happening in a github repository at a glance" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <ThemeProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ThemeProvider>
+        </>
     )
 }
